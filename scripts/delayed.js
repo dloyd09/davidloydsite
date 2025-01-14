d3366fadbc5351b8e2caf3e81c4dfb5dd1cecebd
dloyd09 <38634715+dloyd09@users.noreply.github.com>
2025-01-14 11:56:52 -0500

    nav bar adjustments

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -6,4 +6,11 @@
   script.integrity = 'sha384-kQtW33rZJAHjgefvhyyzcGF8Tx82fJb5hAKhuXW2iRFoZKUtD3r4LFp2kg53Ugf8';
   script.crossOrigin = 'anonymous';
   document.body.appendChild(script);
+
+  // Load Font Awesome CSS
+  const fontAwesomeLink = document.createElement('link');
+  fontAwesomeLink.rel = 'stylesheet';
+  fontAwesomeLink.href = 'https://kit.fontawesome.com/0d64d392b4.js';
+  fontAwesomeLink.crossOrigin = 'anonymous';
+  document.head.appendChild(fontAwesomeLink);
 }());
