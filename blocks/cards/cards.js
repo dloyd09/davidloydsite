75a8967372f8c3c547b3729256bdeb1ac6c2687f
dloyd09 <38634715+dloyd09@users.noreply.github.com>
2025-01-13 19:46:13 -0500

    updates

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -54,32 +54,32 @@ export default function decorate(block) {
     // cardFooter.classList.add('d-flex', 'justify-content-between', 'align-items-center');
 
     // Buttons
-    //  const btnGroup = document.createElement('div');
-    //  btnGroup.classList.add('btn-group');
+    // const btnGroup = document.createElement('div');
+    // btnGroup.classList.add('btn-group');
 
-    //  const viewBtn = document.createElement('button');
-    //  viewBtn.type = 'button';
-    //  viewBtn.classList.add('btn', 'btn-sm', 'btn-outline-secondary');
-    //  viewBtn.textContent = 'View';
+    // const viewBtn = document.createElement('button');
+    // viewBtn.type = 'button';
+    // viewBtn.classList.add('btn', 'btn-sm', 'btn-outline-secondary');
+    // viewBtn.textContent = 'View';
   
-    //  const editBtn = document.createElement('button');
-    //  editBtn.type = 'button';
-    //  editBtn.classList.add('btn', 'btn-sm', 'btn-outline-secondary');
-    //  editBtn.textContent = 'Edit';
+    // const editBtn = document.createElement('button');
+    // editBtn.type = 'button';
+    // editBtn.classList.add('btn', 'btn-sm', 'btn-outline-secondary');
+    // editBtn.textContent = 'Edit';
   
-    //  btnGroup.appendChild(viewBtn);
-    //  btnGroup.appendChild(editBtn);
-    //  cardFooter.appendChild(btnGroup);
+    // btnGroup.appendChild(viewBtn);
+    // btnGroup.appendChild(editBtn);
+    // cardFooter.appendChild(btnGroup);
   
-    //   Time
-    //  if (card.time) {
-    //    const smallText = document.createElement('small');
-    //    smallText.classList.add('text-muted');
-    //    smallText.textContent = card.time;
-    //    cardFooter.appendChild(smallText);
+    // Time
+    // if (card.time) {
+    //   const smallText = document.createElement('small');
+    //   smallText.classList.add('text-muted');
+    //   smallText.textContent = card.time;
+    //   cardFooter.appendChild(smallText);
     //  }
   
-    //  cardBody.appendChild(cardFooter);
+    // cardBody.appendChild(cardFooter);
     cardElement.appendChild(cardBody);
     col.appendChild(cardElement);
     row.appendChild(col);
